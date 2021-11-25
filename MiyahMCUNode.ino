19bb18b3461e0076295954495e99fbc886e126a0
Mohammed <37247525+mhmdcs@users.noreply.github.com>
2021-11-25 15:27:08 +0300

    updating comments

--- MiyahMCUNode.ino
+++ MiyahMCUNode.ino
@@ -6,22 +6,20 @@
 #include <FirebaseArduino.h>
 #include <FirebaseHttpClient.h>
 
-//set up firebase connection with link of the firebase databse  for FIREBASE_HOST and secret key authorization for FIREBASE_AUTH
+//set up firebase connection variables with link of the firebase databse  for FIREBASE_HOST and secret key authorization for FIREBASE_AUTH
 #define FIREBASE_HOST "miyah-application-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "zVPxt4bgSASoD6hp5Z4eaYCVM8H0Uo9EJeHIYzAY" //firebase database seceret key
-//set up wifi connection
+//set up wifi connection variables
 #define WIFI_SSID "note20mhmd" //wifi network name 
 #define WIFI_PASSWORD "20406080" //wifi password
 
 
 //define pins for the ultrasonic sensor (trig and echo pins), and the variables for the water level calculations and data
-String sensorData;
-char rdata;
+
 const int trigPin = D5;
 const int echoPin = D6;
-int firstVal;
- long duration;  
- int distance;  
+long duration;  
+int distance;  
 
 
 void setup() { //setup method only runs code once
@@ -35,7 +33,7 @@ void setup() { //setup method only runs code once
   //source for setting up the firebase-arduino connection https://www.instructables.com/Firebase-Integrate-With-ESP8266/
 
   //establish the wifi and firebase connection
-pinMode(16, OUTPUT);
+pinMode(16, OUTPUT); //pin 16 is used to wake up the ESP8266 from deep sleep https://user-images.githubusercontent.com/53692075/111992062-00578880-8b3b-11eb-9eab-59f927277be4.png // https://randomnerdtutorials.com/esp8266-deep-sleep-with-arduino-ide/#:~:text=GPIO%2016%20must%20be%20connected,pin%20receives%20a%20LOW%20signal.   
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while(WiFi.status() != WL_CONNECTED)
@@ -49,7 +47,8 @@ pinMode(16, OUTPUT);
 
   //begin the  firebase connection
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.setInt("distance", 0);
+  Firebase.setInt("users/TEZLRyoqtwUuJHgRx4IhBWzFbC42/espDistance", 0);
+  
 }//end of setup method
 
 
@@ -70,13 +69,13 @@ digitalWrite(trigPin, LOW);
 //reads the echoPin and then returns the sound wave frequencies travel-time in microseconds 
 duration = pulseIn(echoPin, HIGH);
 
-//calculates the distance, source https://simple-circuit.com/arduino-hc-sr04-sensor-distance-meter/
+//calculates the distance, source  https://www.thegeekpub.com/235425/arduino-ultrasonic-sensor-tutorial/  &&  https://simple-circuit.com/arduino-hc-sr04-sensor-distance-meter/
 distance= duration*0.034/2;  
 
 //prints the distance on the Ardunio IDE's serial monitor 
 Serial.print("Distance: ");
 Serial.println(distance);
-Firebase.setInt("distance",distance);
+Firebase.setInt("users/TEZLRyoqtwUuJHgRx4IhBWzFbC42/espDistance",distance);
 delay(2000);
 
 }//end of loop method
