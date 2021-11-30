f89e4235186fb3e4c2d9717c26358f09580409b6
Mohammed <37247525+mhmdcs@users.noreply.github.com>
2021-11-30 18:03:52 +0300

    update original to display abdulrhaman's changes

--- MiyahMCUNode.ino
+++ MiyahMCUNode.ino
@@ -1,5 +1,3 @@
-
-
 //importing all the necessary libraries, the ESP8266 MCU node wifi support library, the software serial, and the firebase implementation libraries to establish the connection between the ESP8266 and the Firebase database
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
@@ -20,6 +18,23 @@ const int trigPin = D5;
 const int echoPin = D6;
 long duration;  
 int distance;  
+
+
+//variables for calculating hourly, weekly and monthly water consumption statistics 
+int water_level_PD [4];
+int pd_temp = 0;
+int pd_avg = 0;
+int pd_sizeofArray = (sizeof(water_level_PD) / sizeof(water_level_PD[0]));
+
+int water_level_PW [4];
+int pw_temp = 0;
+int pw_avg = 0;
+int pw_sizeofArray = (sizeof(water_level_PW) / sizeof(water_level_PW[0]));
+int pw_check = 0;
+
+int ph_temp = 0;
+int water_level_PH [4];
+int ph_avg = 0;
 
 
 void setup() { //setup method only runs code once
@@ -57,6 +72,10 @@ void loop() {//loop method is where the main code logic that's going to be repea
 
 //source for clearing trigPin and setting it HIGH http://electronoobs.com/eng_arduino_tut80.php
 
+
+//loop to add the current level to the array
+for (int i = 0; i < (sizeof(water_level_PH) / sizeof(water_level_PH[0])); i++){
+
 //clear the trigPin
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
@@ -75,7 +94,128 @@ distance= duration*0.034/2;
 //prints the distance on the Ardunio IDE's serial monitor 
 Serial.print("Distance: ");
 Serial.println(distance);
-Firebase.setInt("users/TEZLRyoqtwUuJHgRx4IhBWzFbC42/espDistance",distance);
-delay(2000);
+Firebase.setInt("users/OXLxTluUlyW4POVaE0womwErvhD3/espDistance",distance);
+    water_level_PH[i] = distance;
+    Serial.print("Current index: ");
+    Serial.print(i);
+            Serial.print(", Index value: " );
+       Serial.println(water_level_PH[i]);
+       delay(10000);
+
+           }
+
+
+
+
+
+
+
+
+    //to calculate the average
+    
+     
+    
+      for(int j = 0; j < (ph_sizeofArray); j++){
+        Serial.print("ph_index ");
+        Serial.print(j);
+        Serial.print(" = ");
+        Serial.println(water_level_PH [j]);
+        
+
+         ph_temp += water_level_PH [j];
+         Serial.print("temp = ");
+       Serial.println(ph_temp);
+        }
+        ph_avg  = ph_temp/(ph_sizeofArray);
+        Serial.print("Average: ");
+       Serial.println(ph_avg);
+       Firebase.setInt("users/OXLxTluUlyW4POVaE0womwErvhD3/avgPH",ph_avg);
+       ph_temp = 0;
+
+       for(int i = 0; i < (pd_sizeofArray) ; i++){
+        if (water_level_PD[i] == 0){
+//          Serial.print(" number: ");
+//          Serial.println(i);
+          water_level_PD[i] = ph_avg;
+          Serial.print("Index number: ");
+          Serial.print(i);
+          Serial.print("  Value: ");
+       Serial.println(water_level_PD [i]);
+          break;          }
+
+        }
+        if(water_level_PD[3] > 0){
+          Serial.print(water_level_PD[3]);
+                for(int j = 0; j < (pd_sizeofArray); j++){
+        Serial.print("index ");
+        Serial.print(j);
+        Serial.print(" =");
+        Serial.println(water_level_PD [j]);
+        
+         pd_temp += water_level_PD [j];
+         Serial.print("pd_temp = ");
+       Serial.println(pd_temp);
+       pd_avg = pd_temp /  (pd_sizeofArray);
+       pw_check =1;
+
+      
+
+       
+        }
+      Serial.print("Day avg= ");
+       Serial.println(pd_avg);
+       Firebase.setInt("users/OXLxTluUlyW4POVaE0womwErvhD3/avgPD",pd_avg);
+       pd_temp = 0;
+   
+        for(int j = 0; j < (pd_sizeofArray); j++){
+         water_level_PD [j] = 0;
+          }
+          }
+          if(pw_check == 1){
+           for(int i = 0; i < (pw_sizeofArray) ; i++){
+        if (water_level_PW[i] == 0){
+//          Serial.print(" number: ");
+//          Serial.println(i);
+          water_level_PW[i] = pd_avg;
+          Serial.print("PW Index number: ");
+          Serial.print(i);
+          Serial.print("  Value: ");
+       Serial.println(water_level_PW [i]);
+       pw_check = 0;
+          break;          }
+
+        }
+        if(water_level_PW[3] > 0){
+        for(int j = 0; j < (pw_sizeofArray); j++){
+        Serial.print("index ");
+        Serial.print(j);
+        Serial.print(" =");
+        Serial.println(water_level_PW [j]);
+        
+         pw_temp += water_level_PW [j];
+         Serial.print("pw_temp = ");
+       Serial.println(pw_temp);
+       pw_avg = pw_temp /  (pw_sizeofArray);
+
+      
+
+       
+        }
+      Serial.print("Week avg= ");
+       Serial.println(pw_avg);
+       Firebase.setInt("users/OXLxTluUlyW4POVaE0womwErvhD3/avgPW",pw_avg);
+       pw_temp = 0;
+        for(int j = 0; j < (pw_sizeofArray); j++){
+         water_level_PW [j] = 0;
+          }
+          }
+
+        }
+
+
+
+
+
+           
 
 }//end of loop method
